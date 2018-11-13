271ed3c481621ff4b30ea9c81a256366deefe3ce
Yanek <Yanek@Yanek-Notebook.fritz.box>
2018-11-13 22:15:37 +0100

    Klassen mit Klassendiagramm abgebildet und Getter/Setter-Methoden.

--- src/Models/Pruefung.java
+++ src/Models/Pruefung.java
@@ -1,5 +1,27 @@
 package Models;
 
 public class Pruefung {
+	
+	private String bezeichnung;
+	private int dauer;	//wenn pro Punkt, 1 Minute Zeit, dann dauer = gesamtpunktzahl
 
+	public Pruefung() {
+		
+	}
+
+	public String getBezeichnung() {
+		return bezeichnung;
+	}
+
+	public void setBezeichnung(String bezeichnung) {
+		this.bezeichnung = bezeichnung;
+	}
+
+	public int getDauer() {
+		return dauer;
+	}
+
+	public void setDauer(int dauer) {
+		this.dauer = dauer;
+	}
 }
