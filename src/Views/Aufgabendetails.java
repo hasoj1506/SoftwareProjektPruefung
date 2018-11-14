8f52c6ceb40d2a962acd839c64f19eff44f9a733
Yanek <Yanek@Yanek-Notebook.fritz.box>
2018-11-14 17:51:10 +0100

    Aufgabendetails: JTable und ein Pane vergrößert.

--- src/Views/Aufgabendetails.java
+++ src/Views/Aufgabendetails.java
@@ -1,10 +1,6 @@
 package Views;
 import javax.swing.*;
-import java.awt.BorderLayout;
-import java.awt.GridBagLayout;
-import java.awt.FlowLayout;
-import java.awt.GridBagConstraints;
-import java.awt.Insets;
+import java.awt.*;
 
 
 public class Aufgabendetails {
@@ -19,6 +15,88 @@ public class Aufgabendetails {
 	public Aufgabendetails() {
 		
 		this.frame = new JFrame("Aufgabendetails");
+		
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.WEST);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JLabel label = new JLabel("Aufgabentitel:");
+		label.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_label = new GridBagConstraints();
+		gbc_label.anchor = GridBagConstraints.EAST;
+		gbc_label.insets = new Insets(0, 0, 5, 5);
+		gbc_label.gridx = 7;
+		gbc_label.gridy = 5;
+		panel.add(label, gbc_label);
+		
+		textField = new JTextField();
+		textField.setHorizontalAlignment(SwingConstants.LEFT);
+		textField.setColumns(10);
+		GridBagConstraints gbc_textField = new GridBagConstraints();
+		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField.insets = new Insets(0, 0, 5, 0);
+		gbc_textField.gridx = 8;
+		gbc_textField.gridy = 5;
+		panel.add(textField, gbc_textField);
+		
+		JLabel label_1 = new JLabel("Aufgabentext:");
+		label_1.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_label_1 = new GridBagConstraints();
+		gbc_label_1.anchor = GridBagConstraints.EAST;
+		gbc_label_1.insets = new Insets(0, 0, 5, 5);
+		gbc_label_1.gridx = 7;
+		gbc_label_1.gridy = 6;
+		panel.add(label_1, gbc_label_1);
+		
+		textField_1 = new JTextField();
+		textField_1.setHorizontalAlignment(SwingConstants.LEFT);
+		textField_1.setColumns(10);
+		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
+		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_1.gridx = 8;
+		gbc_textField_1.gridy = 6;
+		panel.add(textField_1, gbc_textField_1);
+		
+		JLabel label_2 = new JLabel("Punktzahl:");
+		label_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_label_2 = new GridBagConstraints();
+		gbc_label_2.anchor = GridBagConstraints.EAST;
+		gbc_label_2.insets = new Insets(0, 0, 5, 5);
+		gbc_label_2.gridx = 7;
+		gbc_label_2.gridy = 7;
+		panel.add(label_2, gbc_label_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setHorizontalAlignment(SwingConstants.LEFT);
+		textField_2.setColumns(10);
+		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_2.insets = new Insets(0, 0, 5, 0);
+		gbc_textField_2.gridx = 8;
+		gbc_textField_2.gridy = 7;
+		panel.add(textField_2, gbc_textField_2);
+		
+		JLabel label_3 = new JLabel("Antworten:");
+		label_3.setHorizontalAlignment(SwingConstants.RIGHT);
+		GridBagConstraints gbc_label_3 = new GridBagConstraints();
+		gbc_label_3.insets = new Insets(0, 0, 0, 5);
+		gbc_label_3.gridx = 7;
+		gbc_label_3.gridy = 8;
+		panel.add(label_3, gbc_label_3);
+		
+		table = new JTable();
+		table.setPreferredSize(new Dimension(150, 400));
+		GridBagConstraints gbc_table = new GridBagConstraints();
+		gbc_table.fill = GridBagConstraints.BOTH;
+		gbc_table.gridx = 8;
+		gbc_table.gridy = 8;
+		panel.add(table, gbc_table);
 		
 		JPanel panel_1 = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
@@ -52,90 +130,6 @@ public class Aufgabendetails {
 		
 		JButton btnNewButton_4 = new JButton("L\u00F6schen");
 		panel_3.add(btnNewButton_4);
-		
-		JPanel panel_4 = new JPanel();
-		frame.getContentPane().add(panel_4, BorderLayout.WEST);
-		
-		JPanel panel = new JPanel();
-		panel_4.add(panel);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 3};
-		gbl_panel.rowHeights = new int[] {0, 0, 0, 0, 0, 0, 0, 0, 0, 4};
-		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
-		
-		JLabel lblNewLabel = new JLabel("Aufgabentitel:");
-		lblNewLabel.setHorizontalAlignment(SwingConstants.RIGHT);
-		GridBagConstraints gbc_lblNewLabel = new GridBagConstraints();
-		gbc_lblNewLabel.anchor = GridBagConstraints.EAST;
-		gbc_lblNewLabel.insets = new Insets(0, 0, 5, 5);
-		gbc_lblNewLabel.gridx = 7;
-		gbc_lblNewLabel.gridy = 5;
-		panel.add(lblNewLabel, gbc_lblNewLabel);
-		
-		textField = new JTextField();
-		textField.setHorizontalAlignment(SwingConstants.LEFT);
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 0);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 8;
-		gbc_textField.gridy = 5;
-		panel.add(textField, gbc_textField);
-		textField.setColumns(10);
-		
-		JLabel lblAufgabentext = new JLabel("Aufgabentext:");
-		lblAufgabentext.setHorizontalAlignment(SwingConstants.RIGHT);
-		GridBagConstraints gbc_lblAufgabentext = new GridBagConstraints();
-		gbc_lblAufgabentext.anchor = GridBagConstraints.EAST;
-		gbc_lblAufgabentext.insets = new Insets(0, 0, 5, 5);
-		gbc_lblAufgabentext.gridx = 7;
-		gbc_lblAufgabentext.gridy = 6;
-		panel.add(lblAufgabentext, gbc_lblAufgabentext);
-		
-		textField_1 = new JTextField();
-		textField_1.setHorizontalAlignment(SwingConstants.LEFT);
-		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
-		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_1.gridx = 8;
-		gbc_textField_1.gridy = 6;
-		panel.add(textField_1, gbc_textField_1);
-		textField_1.setColumns(10);
-		
-		JLabel lblPunktzahl = new JLabel("Punktzahl:");
-		lblPunktzahl.setHorizontalAlignment(SwingConstants.RIGHT);
-		GridBagConstraints gbc_lblPunktzahl = new GridBagConstraints();
-		gbc_lblPunktzahl.anchor = GridBagConstraints.EAST;
-		gbc_lblPunktzahl.insets = new Insets(0, 0, 5, 5);
-		gbc_lblPunktzahl.gridx = 7;
-		gbc_lblPunktzahl.gridy = 7;
-		panel.add(lblPunktzahl, gbc_lblPunktzahl);
-		
-		textField_2 = new JTextField();
-		textField_2.setHorizontalAlignment(SwingConstants.LEFT);
-		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
-		gbc_textField_2.insets = new Insets(0, 0, 5, 0);
-		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_2.gridx = 8;
-		gbc_textField_2.gridy = 7;
-		panel.add(textField_2, gbc_textField_2);
-		textField_2.setColumns(10);
-		
-		JLabel lblAntworten = new JLabel("Antworten:");
-		lblAntworten.setHorizontalAlignment(SwingConstants.RIGHT);
-		GridBagConstraints gbc_lblAntworten = new GridBagConstraints();
-		gbc_lblAntworten.insets = new Insets(0, 0, 0, 5);
-		gbc_lblAntworten.gridx = 7;
-		gbc_lblAntworten.gridy = 8;
-		panel.add(lblAntworten, gbc_lblAntworten);
-		
-		table = new JTable();
-		GridBagConstraints gbc_table = new GridBagConstraints();
-		gbc_table.fill = GridBagConstraints.BOTH;
-		gbc_table.gridx = 8;
-		gbc_table.gridy = 8;
-		panel.add(table, gbc_table);
 		
 		
 		
