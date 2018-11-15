030801b8bbd4943d1a759ac5275ff6dfcc091bf5
Yanek <Yanek@Yanek-Notebook.fritz.box>
2018-11-15 23:21:25 +0100

    Aufgabendetails weitergemacht.

--- src/Views/Aufgabendetails.java
+++ src/Views/Aufgabendetails.java
@@ -1,7 +1,10 @@
 package Views;
-import javax.swing.*;
-import java.awt.*;
 
+import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import javax.swing.table.DefaultTableModel;
+
+import java.awt.*;
 
 public class Aufgabendetails {
 
@@ -10,25 +13,24 @@ public class Aufgabendetails {
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTable table;
-	
-	
+
 	public Aufgabendetails() {
 		onCreate();
 	}
-	
+
 	public void onCreate() {
-		
-		JFrame frame = new JFrame("Aufgabendetails");
-		
+
+		this.frame = new JFrame("Aufgabendetails");
+
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.WEST);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 50.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.columnWidths = new int[] { 0, 0, 0, 492, 0, 0 };
+		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_panel.columnWeights = new double[] { 1.0, 0.0, 0.0, 1.0, 0.0, Double.MIN_VALUE };
+		gbl_panel.rowWeights = new double[] { 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
 		panel.setLayout(gbl_panel);
-		
+
 		JLabel label = new JLabel("Aufgabentitel:");
 		label.setHorizontalAlignment(SwingConstants.RIGHT);
 		GridBagConstraints gbc_label = new GridBagConstraints();
@@ -37,7 +39,7 @@ public class Aufgabendetails {
 		gbc_label.gridx = 2;
 		gbc_label.gridy = 3;
 		panel.add(label, gbc_label);
-		
+
 		textField = new JTextField();
 		textField.setHorizontalAlignment(SwingConstants.LEFT);
 		textField.setColumns(10);
@@ -47,13 +49,12 @@ public class Aufgabendetails {
 		gbc_textField.weightx = 0.6;
 		gbc_textField.ipadx = 50;
 		gbc_textField.anchor = GridBagConstraints.LINE_END;
-		gbc_textField.gridwidth = 2;
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField.insets = new Insets(0, 0, 5, 5);
 		gbc_textField.gridx = 3;
 		gbc_textField.gridy = 3;
 		panel.add(textField, gbc_textField);
-		
+
 		JLabel label_1 = new JLabel("Aufgabentext:");
 		label_1.setHorizontalAlignment(SwingConstants.RIGHT);
 		GridBagConstraints gbc_label_1 = new GridBagConstraints();
@@ -62,20 +63,20 @@ public class Aufgabendetails {
 		gbc_label_1.gridx = 2;
 		gbc_label_1.gridy = 4;
 		panel.add(label_1, gbc_label_1);
-		
+
 		textField_1 = new JTextField();
 		textField_1.setHorizontalAlignment(SwingConstants.LEFT);
 		textField_1.setColumns(10);
 		textField_1.setPreferredSize(new Dimension(100, 115));
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.anchor = GridBagConstraints.NORTH;
-		gbc_textField_1.gridwidth = 2;
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textField_1.weighty = 1.0;
+		gbc_textField_1.weightx = 1.0;
 		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
 		gbc_textField_1.gridx = 3;
 		gbc_textField_1.gridy = 4;
 		panel.add(textField_1, gbc_textField_1);
-		
+
 		JLabel label_2 = new JLabel("Punktzahl:");
 		label_2.setHorizontalAlignment(SwingConstants.RIGHT);
 		GridBagConstraints gbc_label_2 = new GridBagConstraints();
@@ -84,17 +85,18 @@ public class Aufgabendetails {
 		gbc_label_2.gridx = 2;
 		gbc_label_2.gridy = 5;
 		panel.add(label_2, gbc_label_2);
-		
+
 		textField_2 = new JTextField();
 		textField_2.setHorizontalAlignment(SwingConstants.LEFT);
 		textField_2.setColumns(10);
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
+		gbc_textField_2.anchor = GridBagConstraints.WEST;
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
+		gbc_textField_2.insets = new Insets(0, 0, 5, 600);
 		gbc_textField_2.gridx = 3;
 		gbc_textField_2.gridy = 5;
 		panel.add(textField_2, gbc_textField_2);
-		
+
 		JLabel label_3 = new JLabel("Antworten:");
 		label_3.setVerticalAlignment(SwingConstants.TOP);
 		label_3.setHorizontalAlignment(SwingConstants.RIGHT);
@@ -104,88 +106,98 @@ public class Aufgabendetails {
 		gbc_label_3.gridx = 2;
 		gbc_label_3.gridy = 7;
 		panel.add(label_3, gbc_label_3);
-		
+
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+		scrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
+		gbc_scrollPane.weightx = 0.5;
+		gbc_scrollPane.insets = new Insets(0, 0, 5, 5);
+		gbc_scrollPane.fill = GridBagConstraints.BOTH;
+		gbc_scrollPane.gridx = 3;
+		gbc_scrollPane.gridy = 7;
+		panel.add(scrollPane, gbc_scrollPane);
+
 		table = new JTable();
-		GridBagConstraints gbc_table = new GridBagConstraints();
-		gbc_table.gridwidth = 2;
-		gbc_table.insets = new Insets(0, 0, 5, 5);
-		gbc_table.fill = GridBagConstraints.BOTH;
-		gbc_table.gridx = 3;
-		gbc_table.gridy = 7;
-		panel.add(table, gbc_table);
-		
+		table.setMinimumSize(new Dimension(500, 500));
+		table.setBorder(new TitledBorder(null, "", TitledBorder.LEADING, TitledBorder.TOP, null, null));
+		scrollPane.setViewportView(table);
+		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		table.setModel(new DefaultTableModel(
+				new Object[][] { { null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null },
+						{ null, null, null }, { null, null, null }, { null, null, null }, { null, null, null }, },
+				new String[] { "Nr.", "Antwort", "Richtig/Falsch" }));
+
 		JPanel panel_4 = new JPanel();
 		GridBagConstraints gbc_panel_4 = new GridBagConstraints();
 		gbc_panel_4.anchor = GridBagConstraints.NORTHEAST;
 		gbc_panel_4.insets = new Insets(0, 0, 5, 0);
 		panel_4.setPreferredSize(new Dimension(100, 200));
 		gbc_panel_4.fill = GridBagConstraints.BOTH;
-		gbc_panel_4.gridx = 6;
+		gbc_panel_4.gridx = 4;
 		gbc_panel_4.gridy = 7;
 		panel.add(panel_4, gbc_panel_4);
 		GridBagLayout gbl_panel_4 = new GridBagLayout();
-		gbl_panel_4.columnWidths = new int[] {20};
-		gbl_panel_4.rowHeights = new int[]{23, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_4.columnWeights = new double[]{0.0};
-		gbl_panel_4.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
+		gbl_panel_4.columnWidths = new int[] { 20 };
+		gbl_panel_4.rowHeights = new int[] { 23, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+		gbl_panel_4.columnWeights = new double[] { 0.0 };
+		gbl_panel_4.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0,
+				Double.MIN_VALUE };
 		panel_4.setLayout(gbl_panel_4);
-		
+
 		JButton btnNewButton_2 = new JButton("Neu");
 		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
-		gbc_btnNewButton_2.ipadx = 34;
+		gbc_btnNewButton_2.ipadx = 33;
 		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 0);
 		gbc_btnNewButton_2.anchor = GridBagConstraints.NORTHWEST;
 		gbc_btnNewButton_2.gridx = 0;
-		gbc_btnNewButton_2.gridy = 4;
+		gbc_btnNewButton_2.gridy = 11;
 		panel_4.add(btnNewButton_2, gbc_btnNewButton_2);
-		
+
 		JButton btnNewButton_3 = new JButton("Bearbeiten");
 		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
-		gbc_btnNewButton_3.ipadx = 20;
 		gbc_btnNewButton_3.insets = new Insets(0, 0, 5, 0);
 		gbc_btnNewButton_3.gridx = 0;
-		gbc_btnNewButton_3.gridy = 5;
+		gbc_btnNewButton_3.gridy = 12;
 		panel_4.add(btnNewButton_3, gbc_btnNewButton_3);
-		
+
 		JButton btnNewButton_4 = new JButton("L\u00F6schen");
 		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
 		gbc_btnNewButton_4.ipadx = 14;
 		gbc_btnNewButton_4.anchor = GridBagConstraints.NORTHWEST;
 		gbc_btnNewButton_4.gridx = 0;
-		gbc_btnNewButton_4.gridy = 6;
+		gbc_btnNewButton_4.gridy = 13;
 		panel_4.add(btnNewButton_4, gbc_btnNewButton_4);
-		
+
 		JPanel panel_1 = new JPanel();
 		FlowLayout flowLayout = (FlowLayout) panel_1.getLayout();
 		flowLayout.setAlignment(FlowLayout.LEFT);
 		frame.getContentPane().add(panel_1, BorderLayout.NORTH);
-		
+
 		JButton btnNewButton_1 = new JButton("Speichern");
-		btnNewButton_1.setIcon(new ImageIcon(Aufgabendetails.class.getResource("/javax/swing/plaf/metal/icons/ocean/floppy.gif")));
+		btnNewButton_1.setIcon(
+				new ImageIcon(Aufgabendetails.class.getResource("/javax/swing/plaf/metal/icons/ocean/floppy.gif")));
 		panel_1.add(btnNewButton_1);
-		
+
 		JButton btnNewButton = new JButton("L\u00F6schen");
 		panel_1.add(btnNewButton);
-		
-		JPanel panel_2 = new JPanel();
-		FlowLayout flowLayout_1 = (FlowLayout) panel_2.getLayout();
-		flowLayout_1.setAlignment(FlowLayout.RIGHT);
-		frame.getContentPane().add(panel_2, BorderLayout.SOUTH);
-		
+
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
 		frame.pack();
-		
+
 	}
-	
+
 	public static void main(String arg) {
 		Aufgabendetails view = new Aufgabendetails();
 	}
-	
-	
-	
-	
+
 }
-
-
-
