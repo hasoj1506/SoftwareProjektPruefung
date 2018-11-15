41df244472505e406e0b5f429503b35c2e8e7efc
Yanek <Yanek@Yanek-Notebook.fritz.box>
2018-11-15 16:29:57 +0100

    Aufgabendetails weitergemacht.

--- src/Views/Aufgabendetails.java
+++ src/Views/Aufgabendetails.java
@@ -13,16 +13,19 @@ public class Aufgabendetails {
 	
 	
 	public Aufgabendetails() {
-		
+		onCreate();
+	}
 	
+	public void onCreate() {
+		
 		JFrame frame = new JFrame("Aufgabendetails");
 		
 		JPanel panel = new JPanel();
 		frame.getContentPane().add(panel, BorderLayout.WEST);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0, 0, 0, 0};
 		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel.columnWeights = new double[]{1.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel.columnWeights = new double[]{1.0, 0.0, 0.0, 50.0, 1.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_panel.rowWeights = new double[]{1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
 		panel.setLayout(gbl_panel);
 		
@@ -31,7 +34,7 @@ public class Aufgabendetails {
 		GridBagConstraints gbc_label = new GridBagConstraints();
 		gbc_label.anchor = GridBagConstraints.EAST;
 		gbc_label.insets = new Insets(0, 0, 5, 5);
-		gbc_label.gridx = 1;
+		gbc_label.gridx = 2;
 		gbc_label.gridy = 3;
 		panel.add(label, gbc_label);
 		
@@ -39,11 +42,15 @@ public class Aufgabendetails {
 		textField.setHorizontalAlignment(SwingConstants.LEFT);
 		textField.setColumns(10);
 		textField.setPreferredSize(new Dimension(5, 21));
+		textField.setMaximumSize(new Dimension(5, 60));
 		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.gridwidth = 3;
+		gbc_textField.weightx = 0.6;
+		gbc_textField.ipadx = 50;
+		gbc_textField.anchor = GridBagConstraints.LINE_END;
+		gbc_textField.gridwidth = 2;
 		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField.insets = new Insets(0, 0, 5, 5);
-		gbc_textField.gridx = 2;
+		gbc_textField.gridx = 3;
 		gbc_textField.gridy = 3;
 		panel.add(textField, gbc_textField);
 		
@@ -52,7 +59,7 @@ public class Aufgabendetails {
 		GridBagConstraints gbc_label_1 = new GridBagConstraints();
 		gbc_label_1.anchor = GridBagConstraints.NORTHEAST;
 		gbc_label_1.insets = new Insets(0, 0, 5, 5);
-		gbc_label_1.gridx = 1;
+		gbc_label_1.gridx = 2;
 		gbc_label_1.gridy = 4;
 		panel.add(label_1, gbc_label_1);
 		
@@ -61,10 +68,11 @@ public class Aufgabendetails {
 		textField_1.setColumns(10);
 		textField_1.setPreferredSize(new Dimension(100, 115));
 		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.gridwidth = 3;
+		gbc_textField_1.anchor = GridBagConstraints.NORTH;
+		gbc_textField_1.gridwidth = 2;
 		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_1.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_1.gridx = 2;
+		gbc_textField_1.gridx = 3;
 		gbc_textField_1.gridy = 4;
 		panel.add(textField_1, gbc_textField_1);
 		
@@ -73,7 +81,7 @@ public class Aufgabendetails {
 		GridBagConstraints gbc_label_2 = new GridBagConstraints();
 		gbc_label_2.anchor = GridBagConstraints.EAST;
 		gbc_label_2.insets = new Insets(0, 0, 5, 5);
-		gbc_label_2.gridx = 1;
+		gbc_label_2.gridx = 2;
 		gbc_label_2.gridy = 5;
 		panel.add(label_2, gbc_label_2);
 		
@@ -83,7 +91,7 @@ public class Aufgabendetails {
 		GridBagConstraints gbc_textField_2 = new GridBagConstraints();
 		gbc_textField_2.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textField_2.insets = new Insets(0, 0, 5, 5);
-		gbc_textField_2.gridx = 2;
+		gbc_textField_2.gridx = 3;
 		gbc_textField_2.gridy = 5;
 		panel.add(textField_2, gbc_textField_2);
 		
@@ -93,49 +101,49 @@ public class Aufgabendetails {
 		GridBagConstraints gbc_label_3 = new GridBagConstraints();
 		gbc_label_3.anchor = GridBagConstraints.NORTHEAST;
 		gbc_label_3.insets = new Insets(0, 0, 5, 5);
-		gbc_label_3.gridx = 1;
+		gbc_label_3.gridx = 2;
 		gbc_label_3.gridy = 7;
 		panel.add(label_3, gbc_label_3);
 		
 		table = new JTable();
 		GridBagConstraints gbc_table = new GridBagConstraints();
-		gbc_table.gridwidth = 3;
+		gbc_table.gridwidth = 2;
 		gbc_table.insets = new Insets(0, 0, 5, 5);
 		gbc_table.fill = GridBagConstraints.BOTH;
-		gbc_table.gridx = 2;
+		gbc_table.gridx = 3;
 		gbc_table.gridy = 7;
 		panel.add(table, gbc_table);
 		
 		JPanel panel_4 = new JPanel();
 		GridBagConstraints gbc_panel_4 = new GridBagConstraints();
 		gbc_panel_4.anchor = GridBagConstraints.NORTHEAST;
-		gbc_panel_4.insets = new Insets(0, 0, 5, 5);
+		gbc_panel_4.insets = new Insets(0, 0, 5, 0);
 		panel_4.setPreferredSize(new Dimension(100, 200));
 		gbc_panel_4.fill = GridBagConstraints.BOTH;
-		gbc_panel_4.gridx = 5;
+		gbc_panel_4.gridx = 6;
 		gbc_panel_4.gridy = 7;
 		panel.add(panel_4, gbc_panel_4);
 		GridBagLayout gbl_panel_4 = new GridBagLayout();
-		gbl_panel_4.columnWidths = new int[] {20, 20, 20};
+		gbl_panel_4.columnWidths = new int[] {20};
 		gbl_panel_4.rowHeights = new int[]{23, 0, 0, 0, 0, 0, 0, 0};
-		gbl_panel_4.columnWeights = new double[]{0.0, 0.0};
+		gbl_panel_4.columnWeights = new double[]{0.0};
 		gbl_panel_4.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel_4.setLayout(gbl_panel_4);
 		
 		JButton btnNewButton_2 = new JButton("Neu");
 		GridBagConstraints gbc_btnNewButton_2 = new GridBagConstraints();
 		gbc_btnNewButton_2.ipadx = 34;
-		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 5);
+		gbc_btnNewButton_2.insets = new Insets(0, 0, 5, 0);
 		gbc_btnNewButton_2.anchor = GridBagConstraints.NORTHWEST;
-		gbc_btnNewButton_2.gridx = 1;
+		gbc_btnNewButton_2.gridx = 0;
 		gbc_btnNewButton_2.gridy = 4;
 		panel_4.add(btnNewButton_2, gbc_btnNewButton_2);
 		
 		JButton btnNewButton_3 = new JButton("Bearbeiten");
 		GridBagConstraints gbc_btnNewButton_3 = new GridBagConstraints();
 		gbc_btnNewButton_3.ipadx = 20;
-		gbc_btnNewButton_3.insets = new Insets(0, 0, 5, 5);
-		gbc_btnNewButton_3.gridx = 1;
+		gbc_btnNewButton_3.insets = new Insets(0, 0, 5, 0);
+		gbc_btnNewButton_3.gridx = 0;
 		gbc_btnNewButton_3.gridy = 5;
 		panel_4.add(btnNewButton_3, gbc_btnNewButton_3);
 		
@@ -143,8 +151,7 @@ public class Aufgabendetails {
 		GridBagConstraints gbc_btnNewButton_4 = new GridBagConstraints();
 		gbc_btnNewButton_4.ipadx = 14;
 		gbc_btnNewButton_4.anchor = GridBagConstraints.NORTHWEST;
-		gbc_btnNewButton_4.insets = new Insets(0, 0, 0, 5);
-		gbc_btnNewButton_4.gridx = 1;
+		gbc_btnNewButton_4.gridx = 0;
 		gbc_btnNewButton_4.gridy = 6;
 		panel_4.add(btnNewButton_4, gbc_btnNewButton_4);
 		
@@ -165,11 +172,8 @@ public class Aufgabendetails {
 		flowLayout_1.setAlignment(FlowLayout.RIGHT);
 		frame.getContentPane().add(panel_2, BorderLayout.SOUTH);
 		
-		
-		
-		
 		frame.setVisible(true);
-		//frame.setDefaulCloseOperation();
+		frame.setDefaultCloseOperation(frame.DISPOSE_ON_CLOSE);
 		frame.pack();
 		
 	}
