57c399a2703f5057979feb6ba3883e747a39f364
Yanek <Yanek@Yanek-Notebook.fritz.box>
2018-11-24 15:46:56 +0100

    JPA auf Nutzer.

--- src/Models/Nutzer.java
+++ src/Models/Nutzer.java
@@ -9,7 +9,7 @@ public class Nutzer {
 	@JoinColumn(name = "nummer")
 	private int idNummer;	// Dozentennummer oder Matrikelnummer
 	
-	private String vornamen;
+	private String vorname;
 	private String nachname;
 	
 	private String benutzername;
