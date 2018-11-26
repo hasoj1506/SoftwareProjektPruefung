b7493df40cf2554a9a99c6cc405cb4d16af435da
Josah <josah.weber@web.de>
2018-11-26 16:04:33 +0100

    DatenbankTest verändert

--- src/Models/DatenbankTest.java
+++ src/Models/DatenbankTest.java
@@ -7,8 +7,16 @@ public class DatenbankTest {
 		DatabaseService service = DatabaseService.getInstance();
 		
 		Nutzer nutzerA = new Nutzer("Hans", "Dieter", "hDieter","1234",true);
+		Nutzer nutzerB = new Nutzer("Franz", "Rudolf", "hRudolf","3563",true);
+		Nutzer nutzerC = new Nutzer("Werner", "Ulf", "hUlf","3234",true);
+		Nutzer nutzerD = new Nutzer("Fritz", "Meier", "hMeier","1564",true);
+		Nutzer nutzerE = new Nutzer("Hainer", "Karsten", "hKarsten","1222",true);
 		
-		service.persistNutzer(nutzerA);
+//		service.persistNutzer(nutzerA);
+		service.persistNutzer(nutzerB);
+		service.persistNutzer(nutzerC);
+		service.persistNutzer(nutzerD);
+		service.persistNutzer(nutzerE);
 
 	}
 
