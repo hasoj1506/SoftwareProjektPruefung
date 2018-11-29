756c0498e56af3ad106ef5b9e76b3633eca66998
Firat <Firat@10.136.193.34>
2018-11-29 15:41:53 +0100

    Antwort an die DB angebunden

--- src/Models/Antwort.java
+++ src/Models/Antwort.java
@@ -1,14 +1,37 @@
 package Models;
 
+import javax.persistence.*;
+
+@Entity
 public class Antwort {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.AUTO)
+	@JoinColumn (name = "antwortId")
+	private long antwortId;
 	
 	private boolean alsRichtigBeantwortet;
 	private boolean istRichtig;
 	private String antworttext;
 	
+	
+	@ManyToOne
+	private Aufgabe aufgabe; //Zu jeder Aufgabe gibt es 1 oder mehrere Antworten
+		
+	
 	public Antwort() {
 		
+		super();
 	}
+	
+	public Antwort(boolean alsRichtigBeantwortet, boolean istRichtig, String antworttext){
+		super();
+		
+		this.alsRichtigBeantwortet = alsRichtigBeantwortet;
+		this.istRichtig = istRichtig;
+		this.antworttext = antworttext; 
+	}
+	
 
 	public boolean isIstRichtig() {
 		return istRichtig;
@@ -32,6 +55,14 @@ public class Antwort {
 
 	public void setAlsRichtigBeantwortet(boolean alsRichtigBeantwortet) {
 		this.alsRichtigBeantwortet = alsRichtigBeantwortet;
+	}
+	
+	public Aufgabe getAufgabe(){
+		return aufgabe;
+	}
+	
+	public void setAufgabe (Aufgabe aufgabe){
+		this.aufgabe = aufgabe;
 	}
 
 }
