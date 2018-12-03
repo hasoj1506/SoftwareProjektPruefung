056cf52a99612af4af242dbde8b856b5908657bd
Josah <josah.weber@web.de>
2018-12-03 15:34:42 +0100

    PruefungsverwaltungController weitergemacht

--- src/Controller/PruefungsverwaltungController.java
+++ src/Controller/PruefungsverwaltungController.java
@@ -1,37 +1,71 @@
 package Controller;
 
-import java.util.ArrayList;
+import java.util.List;
 
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+
+import Models.DatabaseService;
 import Models.Pruefung;
-import Views.Pruefungsverwaltung;
+import Views.PruefungsverwaltungView;
 
+//Josah Weber
 public class PruefungsverwaltungController {
-	// alle Methoden, die durch Bedienung der Pruefungsverwaltung-View aufgerufen werden können
-	
-	private Pruefungsverwaltung view;
-	private ArrayList<Pruefung> pruefungen;
-	
-	public PruefungsverwaltungController(Pruefungsverwaltung view){
+	// alle Methoden, die durch Bedienung der Pruefungsverwaltung-View
+	// aufgerufen werden können
+
+	private PruefungsverwaltungView view;
+	private List<Pruefung> pruefungen;
+	EntityManager em = DatabaseService.getInstance().getEntityManager();
+
+	public PruefungsverwaltungController(PruefungsverwaltungView view) {
 		this.view = view;
-		this.pruefungen = new ArrayList<Pruefung>();
 	}
-	
-	public void fuelleTabellePruefungsverwaltung(){
-		
+
+	public void fuelleTabellePruefungsverwaltung() {
+
+		// Liste von Pruefungsdatensätzen erstellen
+		try {
+
+			Query q = em.createQuery("select * from pruefung");
+
+			pruefungen = (List<Pruefung>)q.getResultList();
+
+			// Zeile für Zeile die Tabelle füllen
+			for (int i = 0; i < pruefungen.size(); i++) {
+
+				String titel = pruefungen.get(i).getBezeichnung();
+				String termin = "Platzhalter"; // Muss noch ausprogrammiert
+												// werden
+				int dauer = pruefungen.get(i).getDauer();
+				String punkte = "Platzhalter"; // muss noch ausprogrammiert
+												// werden und Datentyp ändern
+				String teilnehmerzahl = "Platzhalter"; // muss noch
+														// ausprogrammiert
+														// werden und Datentyp
+														// ändern
+
+				view.getTableModel().insertRow(view.getTableModel().getRowCount(),
+						new Object[] { titel, termin, dauer, punkte, teilnehmerzahl });
+
+			}
+
+		} catch (Exception e) {
+			// füllen, was beim Fehler passiert
+		}
+
 	}
-	
-	public void erstellePruefung(){
-		
+
+	public void erstellePruefung() {
+
 	}
-	
-	public void editierePruefung(){
-		
+
+	public void editierePruefung() {
+
 	}
-	
-	public void loeschePruefung(){
-		
+
+	public void loeschePruefung() {
+
 	}
-	
-	
 
 }
