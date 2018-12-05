cf35e46ad2bee155cf4f5e47f7e172606eb5367a
vicy <vicy@DESKTOP-B700E8U.dhcp.fh-bielefeld.de>
2018-12-05 11:49:30 +0100

    Prüfungsverwaltung

--- src/Views/LoginPruefungsverwaltung.java
+++ src/Views/LoginPruefungsverwaltung.java
@@ -1,27 +1,24 @@
 package Views;
-
-import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import java.awt.BorderLayout;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+import java.awt.FlowLayout;
 import java.awt.GridBagLayout;
 import java.awt.GridBagConstraints;
-import javax.swing.JTextField;
 import java.awt.Insets;
-import java.awt.Font;
+import javax.swing.SwingConstants;
+import javax.swing.JTextField;
 import javax.swing.JButton;
-import java.awt.Component;
-import java.awt.Point;
-import java.awt.Dimension;
+import java.awt.Font;
 
-public class LoginPruefungsverwaltung extends JFrame {
+public class LoginPruefungsverwaltung {
 
-	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JFrame frame;
+	private JTextField textFieldBenutzername;
+	private JTextField textFieldPasswort;
 
 	/**
 	 * Launch the application.
@@ -30,8 +27,8 @@ public class LoginPruefungsverwaltung extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					LoginPruefungsverwaltung frame = new LoginPruefungsverwaltung();
-					frame.setVisible(true);
+					LoginPruefungsverwaltung window = new LoginPruefungsverwaltung();
+					window.frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -40,102 +37,93 @@ public class LoginPruefungsverwaltung extends JFrame {
 	}
 
 	/**
-	 * Create the frame.
+	 * Create the application.
 	 */
 	public LoginPruefungsverwaltung() {
-		setMinimumSize(new Dimension(300, 200));
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(new BorderLayout(0, 0));
+		initialize();
+	}
 
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+		
 		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.NORTH);
+		frame.getContentPane().add(panel, BorderLayout.NORTH);
 		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[] { 0, 0 };
-		gbl_panel.rowHeights = new int[] { 0, 0, 0, 0 };
-		gbl_panel.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-		gbl_panel.rowWeights = new double[] { 1.0, 1.0, 1.0, Double.MIN_VALUE };
+		gbl_panel.columnWidths = new int[]{0, 0};
+		gbl_panel.rowHeights = new int[] {40, 40};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 0.0};
 		panel.setLayout(gbl_panel);
-
-		JPanel panel_2 = new JPanel();
-		panel_2.setPreferredSize(new Dimension(50, 50));
-		GridBagConstraints gbc_panel_2 = new GridBagConstraints();
-		gbc_panel_2.insets = new Insets(0, 0, 5, 0);
-		gbc_panel_2.fill = GridBagConstraints.BOTH;
-		gbc_panel_2.gridx = 0;
-		gbc_panel_2.gridy = 0;
-		panel.add(panel_2, gbc_panel_2);
-
-		JLabel lblPruefungsverwaltung = new JLabel("Pr\u00FCfungsverwaltung");
-		lblPruefungsverwaltung.setFont(new Font("Tahoma", Font.BOLD, 24));
+		
+		JLabel lblPruefungsverwaltung = new JLabel("Pruefungsverwaltung");
+		lblPruefungsverwaltung.setFont(new Font("Tahoma", Font.PLAIN, 20));
 		GridBagConstraints gbc_lblPruefungsverwaltung = new GridBagConstraints();
 		gbc_lblPruefungsverwaltung.insets = new Insets(0, 0, 5, 0);
 		gbc_lblPruefungsverwaltung.gridx = 0;
-		gbc_lblPruefungsverwaltung.gridy = 1;
+		gbc_lblPruefungsverwaltung.gridy = 0;
 		panel.add(lblPruefungsverwaltung, gbc_lblPruefungsverwaltung);
-
+		
 		JLabel lblVersion = new JLabel("Version");
 		GridBagConstraints gbc_lblVersion = new GridBagConstraints();
+		gbc_lblVersion.anchor = GridBagConstraints.NORTH;
 		gbc_lblVersion.gridx = 0;
-		gbc_lblVersion.gridy = 2;
+		gbc_lblVersion.gridy = 1;
 		panel.add(lblVersion, gbc_lblVersion);
-
+		
 		JPanel panel_1 = new JPanel();
-		contentPane.add(panel_1, BorderLayout.CENTER);
+		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
 		GridBagLayout gbl_panel_1 = new GridBagLayout();
-		gbl_panel_1.columnWidths = new int[] { 0, 0, 0 };
-		gbl_panel_1.rowHeights = new int[] { 0, 0, 0, 0, 0 };
-		gbl_panel_1.columnWeights = new double[] { 0.0, 1.0, Double.MIN_VALUE };
-		gbl_panel_1.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
+		gbl_panel_1.columnWidths = new int[]{0, 0, 0};
+		gbl_panel_1.rowHeights = new int[] {0, 0, 0, 0};
+		gbl_panel_1.columnWeights = new double[]{0.0, 1.0, Double.MIN_VALUE};
+		gbl_panel_1.rowWeights = new double[]{0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel_1.setLayout(gbl_panel_1);
-
+		
 		JLabel lblBenutzername = new JLabel("Benutzername");
-		lblBenutzername.setAlignmentX(Component.CENTER_ALIGNMENT);
 		GridBagConstraints gbc_lblBenutzername = new GridBagConstraints();
-		gbc_lblBenutzername.anchor = GridBagConstraints.EAST;
 		gbc_lblBenutzername.insets = new Insets(0, 0, 5, 5);
+		gbc_lblBenutzername.anchor = GridBagConstraints.EAST;
 		gbc_lblBenutzername.gridx = 0;
-		gbc_lblBenutzername.gridy = 1;
+		gbc_lblBenutzername.gridy = 0;
 		panel_1.add(lblBenutzername, gbc_lblBenutzername);
-
-		textField = new JTextField();
-		GridBagConstraints gbc_textField = new GridBagConstraints();
-		gbc_textField.insets = new Insets(0, 0, 5, 0);
-		gbc_textField.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField.gridx = 1;
-		gbc_textField.gridy = 1;
-		panel_1.add(textField, gbc_textField);
-		textField.setColumns(10);
-
+		
+		textFieldBenutzername = new JTextField();
+		GridBagConstraints gbc_textFieldBenutzername = new GridBagConstraints();
+		gbc_textFieldBenutzername.insets = new Insets(0, 0, 5, 0);
+		gbc_textFieldBenutzername.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFieldBenutzername.gridx = 1;
+		gbc_textFieldBenutzername.gridy = 0;
+		panel_1.add(textFieldBenutzername, gbc_textFieldBenutzername);
+		textFieldBenutzername.setColumns(10);
+		
 		JLabel lblPasswort = new JLabel("Passwort");
 		GridBagConstraints gbc_lblPasswort = new GridBagConstraints();
+		gbc_lblPasswort.anchor = GridBagConstraints.EAST;
 		gbc_lblPasswort.insets = new Insets(0, 0, 5, 5);
 		gbc_lblPasswort.gridx = 0;
-		gbc_lblPasswort.gridy = 2;
+		gbc_lblPasswort.gridy = 1;
 		panel_1.add(lblPasswort, gbc_lblPasswort);
-
-		textField_1 = new JTextField();
-		GridBagConstraints gbc_textField_1 = new GridBagConstraints();
-		gbc_textField_1.insets = new Insets(0, 0, 5, 0);
-		gbc_textField_1.fill = GridBagConstraints.HORIZONTAL;
-		gbc_textField_1.gridx = 1;
-		gbc_textField_1.gridy = 2;
-		panel_1.add(textField_1, gbc_textField_1);
-		textField_1.setColumns(10);
-
+		
+		textFieldPasswort = new JTextField();
+		GridBagConstraints gbc_textFieldPasswort = new GridBagConstraints();
+		gbc_textFieldPasswort.insets = new Insets(0, 0, 5, 0);
+		gbc_textFieldPasswort.fill = GridBagConstraints.HORIZONTAL;
+		gbc_textFieldPasswort.gridx = 1;
+		gbc_textFieldPasswort.gridy = 1;
+		panel_1.add(textFieldPasswort, gbc_textFieldPasswort);
+		textFieldPasswort.setColumns(10);
+		
 		JButton btnLogin = new JButton("Login");
 		GridBagConstraints gbc_btnLogin = new GridBagConstraints();
 		gbc_btnLogin.anchor = GridBagConstraints.EAST;
 		gbc_btnLogin.gridx = 1;
-		gbc_btnLogin.gridy = 3;
+		gbc_btnLogin.gridy = 2;
 		panel_1.add(btnLogin, gbc_btnLogin);
-
-		setLocationRelativeTo(null);
-		setMinimumSize(new Dimension(500, 300));
-		setResizable(false);
 	}
-
 }
