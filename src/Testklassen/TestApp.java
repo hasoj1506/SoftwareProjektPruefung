49fa64c5f9568b9feeda4d50a21651a83508faf3
Firat <Firat@10.136.193.119>
2018-12-07 15:16:52 +0100

    kommentiert

--- src/Testklassen/TestApp.java
+++ src/Testklassen/TestApp.java
@@ -6,12 +6,12 @@ import Views.PruefungsDetails;
 import Views.PruefungsverwaltungView;
 
 public class TestApp {
-
+/*
 	public static void main(String[] args) {
 		Pruefungsverwaltung view = new Pruefungsverwaltung();
 		Aufgabendetails view2 = new Aufgabendetails();
 		PruefungsDetails view3 = new PruefungsDetails();
 		PruefungView view4 = new PruefungView();
 	}
-
+*/
 }
