47af004475e7c6a011b596c45579e0d1bdcb4f6d
Josah <josah.weber@web.de>
2018-12-09 15:15:16 +0100

    test

--- src/Testklassen/yaneksViewTest.java
+++ src/Testklassen/yaneksViewTest.java
@@ -13,31 +13,29 @@ import Views.AufgabendetailsView;
 public class yaneksViewTest {
 
 	public static void main(String[] args) {
-		
+
 		DatabaseService service = DatabaseService.getInstance();
 
 		Pruefung pruefung = new Pruefung("Mathepruefung 1", 120);
-				
+
 		Aufgabe aufgabe3 = new Aufgabe("t", 5, "5", pruefung);
-		
+
 		Antwort antwort1 = new Antwort("text", true, 5, aufgabe3);
 		Antwort antwort2 = new Antwort("te33t", true, 7, aufgabe3);
 		Antwort antwort3 = new Antwort("te44", false, 4, aufgabe3);
-		
+
 		aufgabe3.addAntwort(antwort1);
 		aufgabe3.addAntwort(antwort2);
 		aufgabe3.addAntwort(antwort3);
-		
+
 		pruefung.addAufgabe(aufgabe3);
-		
+
 		service.persistPruefung(pruefung);
-		
+
 		List<Antwort> antworten = service.readAntworten(aufgabe3);
-		
+
 		System.out.println(antworten.size());
-		
-		
-		
+
 	}
 
 }
