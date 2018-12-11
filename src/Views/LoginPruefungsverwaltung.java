eaf99a6c5b384396a19b39fb3f2743d60f10b821
firat <firat@DESKTOP-BSPHA6Q.dhcp.fh-bielefeld.de>
2018-12-11 13:11:20 +0100

    kommentar

--- src/Views/LoginPruefungsverwaltung.java
+++ src/Views/LoginPruefungsverwaltung.java
@@ -58,7 +58,7 @@ public class LoginPruefungsverwaltung {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		
+		//Kommentar
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
