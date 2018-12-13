32a350677d5d0a07a297f8e6c89153a474896f94
vicy <vicy@192.168.0.38>
2018-12-13 10:28:30 +0100

    Login

--- src/Testklassen/DatenbankTest.java
+++ src/Testklassen/DatenbankTest.java
@@ -2,6 +2,7 @@ package Testklassen;
 
 import java.util.ArrayList;
 import DatabaseService.DatabaseService;
+import Models.Nutzer;
 import Models.Pruefung;
 import Models.Termin;
 
@@ -10,26 +11,29 @@ public class DatenbankTest {
 	public static void main(String[] args) {
 
 		DatabaseService service = DatabaseService.getInstance();
+		
+		Nutzer nutzer1 = new Nutzer("Alex", "Foerster", "AFoerster", "1234", true);
+		Nutzer nutzer2 = new Nutzer("Franz", "Mueller", "FMueller", "1234", false);
 
-		for (int i = 0; i < 20; i++) {
-			// erzeugt ein Prüfungsobjekt mit Daten (siehe Konstruktor)
-			Pruefung pruefung = new Pruefung("Mathepruefung 1", 120);
-
-			// erzeugt ein Termin Objekt mit Daten (siehe Konstruktor)
-			Termin termin = new Termin("test", "test", "test", pruefung);
-			Termin termin1 = new Termin("test5", "test4", "test3", pruefung);
-			Termin termin2 = new Termin("test35", "test42", "test2", pruefung);
-
-			// fügt ein Termin in den HashSet hinzu
-			pruefung.addTermin(termin);
-			pruefung.addTermin(termin1);
-			pruefung.addTermin(termin2);
-
-			//
-			// persistiert(in die Datenbank schreiben) die Prüfung in der
-			// Datenbank
-			service.persistPruefung(pruefung);
-		}
+//		for (int i = 0; i < 20; i++) {
+//			// erzeugt ein Prüfungsobjekt mit Daten (siehe Konstruktor)
+//			Pruefung pruefung = new Pruefung("Mathepruefung 1", 120);
+//
+//			// erzeugt ein Termin Objekt mit Daten (siehe Konstruktor)
+//			Termin termin = new Termin("test", "test", "test", pruefung);
+//			Termin termin1 = new Termin("test5", "test4", "test3", pruefung);
+//			Termin termin2 = new Termin("test35", "test42", "test2", pruefung);
+//
+//			// fügt ein Termin in den HashSet hinzu
+//			pruefung.addTermin(termin);
+//			pruefung.addTermin(termin1);
+//			pruefung.addTermin(termin2);
+//
+//			//
+//			// persistiert(in die Datenbank schreiben) die Prüfung in der
+//			// Datenbank
+//			service.persistPruefung(pruefung);
+//		}
 
 	}
 
