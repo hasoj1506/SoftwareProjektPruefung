5e57139efc3051c871ca902e6b9331463e9c1712
Josah <josah.weber@web.de>
2018-12-19 13:58:59 +0100

    nichts geändert, aber muss pushen

--- src/Views/NutzerErstellenPopUp.java
+++ src/Views/NutzerErstellenPopUp.java
@@ -1,18 +1,28 @@
 package Views;
 
-import javax.swing.*;
-import javax.swing.table.DefaultTableModel;
-
-import Models.Antwort;
-import Models.Nutzer;
-import Models.Pruefung;
-import Models.Student;
-import Models.Termin;
-
-import java.awt.*;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+
+import Models.Pruefung;
+import Models.Student;
 
 //Josah Weber
 public class NutzerErstellenPopUp {
