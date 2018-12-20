e5ccb5bb131138966bdd9d5c5555f3317c0bb8b0
Josah <josah.weber@web.de>
2018-12-20 15:33:26 +0100

    testklasse verändert

--- src/Testklassen/JosahTestKlasse.java
+++ src/Testklassen/JosahTestKlasse.java
@@ -1,12 +1,31 @@
 package Testklassen;
 
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import Views.LoginAbfrage;
 import Views.PruefungsverwaltungView;
 
 public class JosahTestKlasse {
 
 	public static void main(String[] args) {
 
-		PruefungsverwaltungView p = new PruefungsverwaltungView();
+		try {
+
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			
+		} catch (UnsupportedLookAndFeelException e) {
+
+		} catch (ClassNotFoundException e) {
+
+		} catch (InstantiationException e) {
+
+		} catch (IllegalAccessException e) {
+
+		};
+
+		
+		PruefungsverwaltungView view = new PruefungsverwaltungView();
 
 	}
 }
