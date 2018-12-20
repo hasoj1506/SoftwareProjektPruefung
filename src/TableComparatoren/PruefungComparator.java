775cf07c0137cf3f5c393d297ece882cc6380ff7
Josah <josah.weber@web.de>
2018-12-20 21:18:04 +0100

    Sinnvolle Sortierungen der Tables

---
+++ src/TableComparatoren/PruefungComparator.java
@@ -0,0 +1,13 @@
+package TableComparatoren;
+
+import java.util.Comparator;
+
+import Models.Pruefung;
+
+public class PruefungComparator implements Comparator<Pruefung> {
+
+	public int compare(Pruefung p1, Pruefung p2) {
+		return p2.getPruefungId() - p1.getPruefungId();
+	}
+
+}
---
+++ src/TableComparatoren/TeilnehmerComparator.java
@@ -0,0 +1,13 @@
+package TableComparatoren;
+
+import java.util.Comparator;
+
+import Models.Student;
+
+public class TeilnehmerComparator implements Comparator<Student> {
+
+	public int compare(Student s1, Student s2) {
+		return s1.getNachname().compareTo(s2.getNachname());
+	}
+
+}
